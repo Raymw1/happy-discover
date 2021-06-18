0ae4df424dcf25e156b5d533ea029462798de929
Rayan Wilbert <rayan.rmw@gmail.com>
2021-06-18 17:33:54 -0300

    Creating fake data

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,1 +1,38 @@
- 
+ module.exports = [
+     {
+         id: 1,
+         name: "Lar das Meninas",
+         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+         images: [
+             "assets/images/home.jpg", 
+             "https://images.unsplash.com/photo-1579196900052-a6034632c042?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3Njc0&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+             "https://images.unsplash.com/photo-1599577180513-7dd76ffd9761?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3Njk2&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+             "https://images.unsplash.com/photo-1572248364230-7f412885f2da?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+             "https://images.unsplash.com/photo-1598454444425-4067fd1ac9db?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzMy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+             "https://images.unsplash.com/photo-1601564267567-f060651e5b2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzU2&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+            ],
+            lat: -22.494188,
+            lng: -43.2065701,
+            instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+            opening_hours: "Horário de visitas Das 8h até 18h",
+            open_on_weekends: "1"
+     },
+     {
+        id: 2,
+        name: "Lar dos Meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1598454444425-4067fd1ac9db?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzMy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+            "https://images.unsplash.com/photo-1599577180513-7dd76ffd9761?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3Njk2&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+            "assets/images/home.jpg", 
+            "https://images.unsplash.com/photo-1579196900052-a6034632c042?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3Njc0&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+            "https://images.unsplash.com/photo-1572248364230-7f412885f2da?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzE4&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit", 
+            "https://images.unsplash.com/photo-1601564267567-f060651e5b2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjI0MDQ3NzU2&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+           ],
+           lat: -22.494188,
+           lng: -43.2165701,
+           instructions: "Blahblahblahblahblah.",
+           opening_hours: "Horário de visitas Das 10h até 20h",
+           open_on_weekends: "0"
+     }
+ ]
